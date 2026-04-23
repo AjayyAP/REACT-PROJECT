2df1e8062545345bd548da704e2982853e185a06
Ajayyap <ajayalakkasseryy@gmail.com>
2026-04-23 16:41:29 +0530

    Update API baseURL to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', 
+  baseURL: 'https://expense-backend-fib9.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
